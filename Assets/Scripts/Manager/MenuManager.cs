3760f937224381a1aa2e7b412dcc888e5dc84da8
hoan <tinhprd@gmail.com>
2025-01-03 11:08:27 +0700

    Add sound menu

--- Assets/Scripts/Manager/MenuManager.cs
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -1,29 +1,79 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject continueButton;
+    public static MenuManager Instance;
+
+    public GameObject continueButton, settingPanel, onBGMButton, offBGMButton, onFXButton, offFXButton;
+    public AudioSource menuBGM, levelBGM, clickFX;
+
+    private bool canPlayBGM, canPlayFX;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (!PlayerPrefs.HasKey("BGM"))
+        {
+            PlayerPrefs.SetInt("BGM", 1);
+        }
+
+        if (!PlayerPrefs.HasKey("FX"))
+        {
+            PlayerPrefs.SetInt("FX", 1);
+        }
+
+        if (PlayerPrefs.GetInt("BGM") == 1)
+        {
+            canPlayBGM = true;
+            onBGMButton.SetActive(true);
+            offBGMButton.SetActive(false);
+        }
+        else
+        {
+            canPlayBGM = false;
+            onBGMButton.SetActive(false);
+            offBGMButton.SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("FX") == 1)
+        {
+            canPlayFX = true;
+            onFXButton.SetActive(true);
+            offFXButton.SetActive(false);
+        }
+        else
+        {
+            canPlayFX = false;
+            onFXButton.SetActive(false);
+            offFXButton.SetActive(true);
+        }
+
+        PlaySound(menuBGM, canPlayBGM);
+
         if (PlayerPrefs.GetInt("Level") > 0)
         {
             continueButton.SetActive(true);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void NewGame()
     {
+        PlaySound(menuBGM, false);
+        PlaySound(clickFX, canPlayFX);
+        PlaySound(levelBGM, canPlayBGM);
         PlayerPrefs.SetInt("Level", 0);
         PlayerPrefs.SetInt("BuySandClock", 0);
         PlayerPrefs.SetInt("BuyPower", 0);
@@ -36,11 +86,71 @@ public class MenuManager : MonoBehaviour
 
     public void Continue()
     {
+        PlaySound(menuBGM, false);
+        PlaySound(clickFX, canPlayFX);
+        PlaySound(levelBGM, canPlayBGM);
         SceneManager.LoadScene("LevelScene");
     }
 
-    public void Setting()
+    public void OpenSetting()
     {
+        PlaySound(clickFX, canPlayFX);
+        settingPanel?.SetActive(true);
+    }
 
+    public void CloseSetting()
+    {
+        PlaySound(clickFX, canPlayFX);
+        settingPanel?.SetActive(false);
+    }
+
+    public void OffBGM()
+    {
+        PlayerPrefs.SetInt("BGM", 1);
+        canPlayBGM = true;
+        PlaySound(clickFX, canPlayFX);
+        PlaySound(menuBGM, canPlayBGM);
+        onBGMButton?.SetActive(true);
+        offBGMButton?.SetActive(false);
+    }
+
+    public void OnBGM()
+    {
+        PlayerPrefs.SetInt("BGM", 0);
+        canPlayBGM = false;
+        PlaySound(clickFX, canPlayFX);
+        PlaySound(menuBGM, canPlayBGM);
+        onBGMButton?.SetActive(false);
+        offBGMButton?.SetActive(true);
+    }
+
+    public void OffFX()
+    {
+        PlayerPrefs.SetInt("FX", 1);
+        canPlayFX = true;
+        PlaySound(clickFX, canPlayFX);
+        onFXButton?.SetActive(true);
+        offFXButton?.SetActive(false);
+    }
+
+    public void OnFX()
+    {
+        PlayerPrefs.SetInt("FX", 0);
+        canPlayFX = false;
+        PlaySound(clickFX, canPlayFX);
+        onFXButton?.SetActive(false);
+        offFXButton?.SetActive(true);
+    }
+
+    public void PlaySound(AudioSource audio, bool canPlay)
+    {
+        if (canPlay)
+        {
+            audio.Play();
+        }
+        else
+        {
+            audio.Stop();
+        }
     }
 }
